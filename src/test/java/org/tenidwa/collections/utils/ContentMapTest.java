14ef67c292ba1d9960891be96c3067d9aaebfa86
Georgy Vlasov <suseika@tendiwa.org>
2015-10-13 18:29:42 +0500

    #25 Add tests for the new methods
    Also added an integration test to check how ContentMap works with AugmentedMap

--- src/test/java/org/tenidwa/collections/utils/ContentMapTest.java
+++ src/test/java/org/tenidwa/collections/utils/ContentMapTest.java
@@ -1,6 +1,7 @@
 package org.tenidwa.collections.utils;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -15,10 +16,7 @@ public final class ContentMapTest {
     @Test
     public void detectsIdenticalValues() {
         final ContentMap<Dude, String, Integer> map = new ContentMap<>(
-            ImmutableMap.of(
-                new CombinedDude("je", "ff"),
-                2
-            ),
+            ImmutableMap.of(new CombinedDude("je", "ff"), 2),
             Dude::name
         );
         MatcherAssert.assertThat(
@@ -28,6 +26,34 @@ public final class ContentMapTest {
         MatcherAssert.assertThat(
             map.get(new PlainDude("jeff")),
             Matchers.equalTo(2)
+        );
+    }
+
+    @Test
+    public void proxiesKeys() {
+        final Dude jeff = new PlainDude("jeff");
+        MatcherAssert.assertThat(
+            new ContentMap<>(
+                ImmutableMap.of(jeff, 2),
+                Dude::name
+            ).keySet(),
+            Matchers.contains(jeff)
+        );
+    }
+
+    @Test
+    public void contentMapAndAugmentedMapWorkTogether() {
+        final Dude jeff = new PlainDude("jeff");
+        MatcherAssert.assertThat(
+            new AugmentedMap<>(
+                new ContentMap<>(
+                    ImmutableMap.of(jeff, 4),
+                    Dude::name
+                ),
+                ImmutableSet.of(jeff, new PlainDude("mac")),
+                dude -> dude.name().length()
+            ),
+            Matchers.hasKey(jeff)
         );
     }
 
